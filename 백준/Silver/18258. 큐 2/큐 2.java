9367cfcd3b8ff401fe84c0a8c8d581ec1fdbcd6e
haeseung <122768657+GOOHAESEUNG@users.noreply.github.com>
2025-06-01 21:31:10 +0900

    [Silver IV] Title: 큐 2, Time: 1208 ms, Memory: 407816 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            String[] split = s.split(" ");
+
+            switch (split[0]) {
+                case "push":
+                    queue.add(Integer.parseInt(split[1]));
+                    break;
+                case "pop":
+                    sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.peekFirst()).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb); // 🔥 한 번만 출력
+    }
+}
