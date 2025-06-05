04d6db78c10f2465cd091854b5d5fab3bb5bb113
haeseung <122768657+GOOHAESEUNG@users.noreply.github.com>
2025-06-05 22:53:44 +0900

    [Bronze II] Title: 피보나치 수, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static int fib(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        if (n > 0) dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        System.out.println(fib(n)); // 55
+    }
+}
