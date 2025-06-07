a318e67460deed6adfd1a988b4dd47c46916bebc
haeseung <122768657+GOOHAESEUNG@users.noreply.github.com>
2025-06-07 23:54:31 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 44432 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A); // 이분 탐색을 위해 정렬
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if (binarySearch(A, target)) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) return true;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+}
